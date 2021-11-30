48348867e1cc4270583f8099e8223261d897ebdf
Shah Bhuiyan <shahjacobb@gmail.com>
2021-11-30 17:39:26 -0500

    solved swapCase() using enumerate()

--- swapCase.py
+++ swapCase.py
@@ -1,16 +1,14 @@
 string = "Sinsa was the most memorable district in Seoul for me."
 # trying to make our swap functions
 
-            
-        
 
-# dumbSwap(string)
-
-def pythonicSwap(s):
-    new_list_str = [i.upper() if i.islower() else i.lower() for i in s.split(" ")]
-    new_list_str = " ".join(new_list_str)
-    return new_list_str
-# dumbSwap(string)
-print(pythonicSwap(string))
+def swap(s):
+    new_str = []
+    new_str = [s[i].upper() if s[i].islower() else s[i].lower() for i,v in enumerate(s)]
+    new_str= "".join(new_str)
     
+    return new_str
+
+
+print(swap(string))
     
